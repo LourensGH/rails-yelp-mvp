788327b79fce100a87058afe403c5a406f9fc491
Lourens Kok <lourenskok@gmail.com>
2024-02-28 23:51:06 +0200

    added reviews table and crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'cleaning database...'
+
+puts "creating retaurants..."
+names = ["Finns", "Vibes", "Lou's Place", "Epic Food", "Dogs Breakfast"]
+adds = ["8 lols road", "14 long street", "11 help avenue", "6 something road", "3 OK street"]
+phones = ["082 3782 9321", "098 5673 0982", "026 9375 9846", "638 4859 3037", "378 6365 9026"]
+cats = ["chinese", "italian", "japanese", "french", "belgian"]
+
+index = 0
+
+5.times do
+  Restaurant.create(
+    name: names[index],
+    address: adds[index],
+    phone_number: phones[index],
+    category: cats[index]
+  )
+  index += 1
+end
